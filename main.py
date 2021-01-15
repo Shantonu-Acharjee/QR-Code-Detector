a32e422bdde8155c0e14d0af6b384843dc6b0a63
ShantonuAcharjee <54053134+shantonuacharjee1@users.noreply.github.com>
2021-01-15 16:39:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-
 #img = cv2.imread('Shantonu.png')
 cap = cv2.VideoCapture(0)
 cap.set(3,460)
 cap.set(4,480)
-
 
 with open('MyData.txt') as f:
     myDataList = f.read().splitlines()
@@ -29,3 +27,21 @@ while True:
             index = (240,420)
 
         else:
+            cv2.imshow('Result', img)
+            myOutput = 'Un-Authorized'
+            welcome = 'YOU DO NOT HAVE PERMISSION SIR'
+            myColor = (0,0,255)
+            index = (50,450)
+
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,myColor,4)
+        pts2 = barcode.rect
+        cv2.putText(img,myOutput,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,0.9,myColor,2)
+        cv2.putText(img,welcome,index,cv2.FONT_HERSHEY_SIMPLEX,1,myColor,2)
+    cv2.imshow('Result', img)
+    if cv2.waitKey(1) == ord('d'): # Press 'D' for close the window
+        break
+
+cv2.destroyAllWindows()
+cap.release()
